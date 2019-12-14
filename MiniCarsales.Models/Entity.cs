26f3e58c1f7686b87453ada8b9424cd1fa768d7c
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-15 08:50:29 +1100

    feat: add IEntity interface and Entity base model

---
+++ MiniCarsales.Models/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MiniCarsales.Models
+{
+    public class Entity : IEntity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAT { get; set; }
+    }
+}
---
+++ MiniCarsales.Models/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniCarsales.Models
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+        DateTime CreatedAt { get; set; }
+        DateTime UpdatedAT { get; set; }
+    }
+}
