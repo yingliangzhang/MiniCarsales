650593e7c3cee3a876975292b39c972802e0856b
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-15 08:59:58 +1100

    feat: add CarsController, add services and repositories to dependency injection, add Automapper to dependency injection

---
+++ MiniCarsales.Web/Controllers/CarsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiniCarsales.Services;
+using AutoMapper;
+using MiniCarsales.Models;
+using MiniCarsales.Dtos;
+
+namespace MiniCarsales.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private readonly CarService _carService;
+        private readonly IMapper _mapper;
+
+        public CarsController(CarService carService, IMapper mapper)
+        {
+            _carService = carService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Get()
+        {
+            var cars = await _carService.GetAllAsync();
+            var carsDto = _mapper.Map<List<Car>, List<CarDto>>(cars);
+            return Ok(carsDto);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Create(Car car)
+        {
+            var createdCar = await _carService.CreateAysnc(car);
+            var carDto = _mapper.Map<Car, CarDto>(createdCar);
+            return Ok(carDto);
+        }
+    }
+}
