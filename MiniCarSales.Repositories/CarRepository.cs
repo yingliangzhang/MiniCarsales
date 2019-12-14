ac373aa7b57faf30315f2a9ad0e77d6b401e7cd4
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-15 08:56:51 +1100

    feat: add VehicleRepository and CarRepository

---
+++ MiniCarSales.Repositories/CarRepository.cs
@@ -0,0 +1,15 @@
+﻿using MiniCarsales.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MiniCarsales.Repositories
+{
+    public class CarRepository : VehicleRepository<Car>
+    {
+        public CarRepository(VehiclesContext vehiclesContext) : base(vehiclesContext)
+        {
+
+        }
+    }
+}
---
+++ MiniCarSales.Repositories/VehicleRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MiniCarsales.Models;
+
+namespace MiniCarsales.Repositories
+{
+    public class VehicleRepository<TEntity> : IVehicleRepository<TEntity> where TEntity : Entity, IEntity
+    {
+        private readonly VehiclesContext _vehicleContext;
+
+        public VehicleRepository(VehiclesContext vehicleContext)
+        {
+            _vehicleContext = vehicleContext;
+        }
+
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            _vehicleContext.Set<TEntity>().Add(entity);
+            await _vehicleContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _vehicleContext.Set<TEntity>().ToListAsync();
+        }
+    }
+}
