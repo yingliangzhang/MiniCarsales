5b7c31bf5968274b90c9f22b4f5d05368691f294
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-15 08:58:57 +1100

    feat: add Dto project with VehicleDto and CarDto

---
+++ MiniCarsales.Dtos/ResponseDtos/CarDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace MiniCarsales.Dtos
+{
+    public class CarDto : VehicleDto
+    {
+        public int NumberOfWheels { get; set; }
+        public int NumberOfDoors { get; set; }
+        public string Engine { get; set; }
+        public string BodyType { get; set; }
+    }
+}
---
+++ MiniCarsales.Dtos/ResponseDtos/VehicleDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniCarsales.Dtos
+{
+    public abstract class VehicleDto
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAT { get; set; }
+    }
+}
