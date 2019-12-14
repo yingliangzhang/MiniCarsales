86deef776dd966a1e2b3ee06966bed58f8f3a752
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-15 08:51:50 +1100

    feat: add entity framework and VehiclesContext

---
+++ MiniCarsales.Models/VehiclesContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace MiniCarsales.Models
+{
+    public class VehiclesContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public VehiclesContext(DbContextOptions<VehiclesContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Override SaveChangesAsync to auto populate CreatedAt and UpdatedAt on save
+        /// </summary>
+        /// <param name="acceptAllChangesOnSuccess"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is Entity && (
+                        e.State == EntityState.Added
+                        || e.State == EntityState.Modified));
+
+            foreach (var entityEntry in entries)
+            {
+                ((Entity)entityEntry.Entity).UpdatedAT = DateTime.Now;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((Entity)entityEntry.Entity).CreatedAt = DateTime.Now;
+                }
+            }
+
+            return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+}
