271b8d06f1f61ff526ab0f731492b8e7fd6f764d
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-15 08:56:28 +1100

    feat: add IVehicleRepository interface with getAll and create function

---
+++ MiniCarSales.Repositories/IVehicleRepository.cs
@@ -0,0 +1,12 @@
+﻿using MiniCarsales.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MiniCarsales.Repositories
+{
+    public interface IVehicleRepository<TEntity> where TEntity : IEntity
+    {
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity> CreateAsync(TEntity entity);
+    }
+}
