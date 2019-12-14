2b01cf44bee554cc9b461cb072facbffde081cbc
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-15 08:51:03 +1100

    feat: add Car model extends Vehicle

---
+++ MiniCarsales.Models/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using static MiniCarsales.Models.CustomValidations.CarBodyTypeAttribute;
+
+namespace MiniCarsales.Models
+{
+    public class Car : Vehicle
+    {
+        [Required]
+        [Range(2, 8)]
+        public int NumberOfWheels { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        public int NumberOfDoors { get; set; }
+
+        [Required]
+        public string Engine { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        [ValidCarBodyType]
+        public string BodyType { get; set; }
+    }
+}
