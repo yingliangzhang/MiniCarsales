ed7eec84a193fcb804446a74c69a07c0abc7ee62
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-15 09:00:29 +1100

    feat: add CarDto mapper profile

---
+++ MiniCarsales.Web/Mapper/CarProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using MiniCarsales.Models;
+using MiniCarsales.Dtos;
+
+namespace MiniCarsales.Web.Mapper
+{
+    public class CarProfile : Profile
+    {
+        public CarProfile()
+        {
+            CreateMap<Car, CarDto>();
+        }
+    }
+}
