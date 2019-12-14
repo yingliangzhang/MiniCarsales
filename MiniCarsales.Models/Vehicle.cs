9eeefd91cdf37295569643549bf16884c51fb14b
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-15 08:50:49 +1100

    feaet: add Vehicle abstract model class

---
+++ MiniCarsales.Models/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using static MiniCarsales.Models.CustomValidations.VehicleTypeAttribute;
+
+namespace MiniCarsales.Models
+{
+    public abstract class Vehicle : Entity, IEntity
+    {
+        [Required]
+        [MaxLength(30)]
+        [ValidVehicleType]
+        public string Type { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string Make { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string Model { get; set; }
+    }
+}
