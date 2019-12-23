e0ff0b0a0530931bf79e6b11a9e816d1ffec526f
Leon Zhang <yingliangzhang89@gmail.com>
2019-12-23 16:47:44 +1100

    feat: add IVehicle interface

---
+++ MiniCarsales.Models/IVehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using static MiniCarsales.Models.CustomValidations.VehicleTypeAttribute;
+
+namespace MiniCarsales.Models
+{
+    public interface IVehicle
+    {
+        Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [ValidVehicleType]
+        string Type { get; }
+
+        [Required]
+        [MaxLength(60)]
+        string Make { get; }
+
+        [Required]
+        [MaxLength(60)]
+        string Model { get; }
+
+        DateTime CreatedAt { get; set; }
+        DateTime UpdatedAT { get; set; }
+    }
+}
